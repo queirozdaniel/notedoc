a36d723b30ea53182d4dc683f88f94855af43a33
queirozdaniel <danielqueirozak77@gmail.com>
2022-05-11 22:29:03 -0300

    add: Created Card for Notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div 
+      class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam beatae cum, quaerat laborum laboriosam blanditiis magni eveniet nisi placeat harum!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
